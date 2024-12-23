0a0b16e1e8a85c0b2bef69de299588e1f35f16b0
DBarbosa15987 <dbarbosa15987@gmail.com>
2024-12-23 17:44:52 +0000

    rel_4_inv_4 feito

--- invariantesJson.py
+++ invariantesJson.py
@@ -16,6 +16,7 @@ def rel_4_inv_0(sheet):
     tem de ter uma justificação associada ao PCA."
     """
 
+    # global allErros
     classesN3 = [x for x in sheet if x["nivel"] == 3]
     erros = []
     for classe in classesN3:
@@ -28,9 +29,52 @@ def rel_4_inv_0(sheet):
             else:
                 erros.append(classe["codigo"])
     # print(erros)
-    # global allErros
     # allErros += erros
     return erros
+
+
+i=0
+
+def checkAntissimetrico(sheet,sheetName,rel):
+
+    global allErros
+    cache = {sheetName:sheet}
+    erros = []
+    for classe in sheet:
+        proRels = classe.get("proRel")
+        proRelCods = classe.get("processosRelacionados")
+        if proRels and proRelCods:
+            relacao = [proRelCods[i] for i,x in enumerate(proRels) if x==rel]
+            for c in relacao:
+                fileName = re.search(r'^\d{3}', c).group(0)
+                if fileName not in cache:
+                    with open(f"files/{fileName}.json","r") as f:
+                        cache[fileName] = json.load(f)
+                # Econtrar o processo em questão
+                sintetizado = [x for x in cache[fileName] if x["codigo"]==c]
+
+                # FIXME: A classe menciona uma classe que não existe!
+                if len(sintetizado) == 0:
+                    print("-"*15)
+                    print(classe["codigo"])
+                    print(rel)
+                    print(c)
+                    print("-"*15)
+                    # Por enquanto ignora-se quando não existe
+                    continue
+
+                proRels2 = sintetizado[0].get("proRel")
+                proRelCods2 = sintetizado[0].get("processosRelacionados")
+                if proRelCods2 and proRels2 and (len(proRelCods2)==len(proRels2)):
+                    relacao2 = [proRelCods2[i] for i,x in enumerate(proRels2) if x==rel]
+                    # Se existe a relação `rel` aqui também, não cumpre com o invariante
+                    if classe["codigo"] in relacao2:
+                        erros.append(classe["codigo"])
+
+    print(f"Erros:{len(erros)}")
+    # allErros += erros
+    return erros
+
 
 def rel_4_inv_1_1(sheet,sheetName):
     """
@@ -39,6 +83,7 @@ def rel_4_inv_1_1(sheet,sheetName):
 
     "A relação eCruzadoCom é simétrica."
     """
+    global i,allErros
     cache = {sheetName:sheet}
     erros = []
     for classe in sheet:
@@ -63,7 +108,7 @@ def rel_4_inv_1_1(sheet,sheetName):
                     print("-"*15)
                     # Por enquanto ignora-se quando não existe
                     continue
-                # cruzadoCod = cruzado[0]["codigo"]
+
                 proRels2 = cruzado[0].get("proRel")
                 proRelCods2 = cruzado[0].get("processosRelacionados")
                 if proRelCods2 and proRels2 and (len(proRelCods2)==len(proRels2)):
@@ -71,14 +116,16 @@ def rel_4_inv_1_1(sheet,sheetName):
                     # Se não eCruzadoCom de volta então não cumpre com o invariante
                     if classe["codigo"] not in eCruzadoCom2:
                         erros.append(classe["codigo"])
+                    else:
+                        i+=1
                 # Se não existe então também não contém eCruzadoCom,
                 # e por isso não cumpre com o invariante
                 else:
                     erros.append(classe["codigo"])
+
     # print(f"Erros:{len(erros)}")
+    # allErros += erros
     return erros
-
-
 
 
 def rel_4_inv_3(sheet,sheetName):
@@ -89,43 +136,18 @@ def rel_4_inv_3(sheet,sheetName):
     "A relação eSintetizadoPor é antisimétrica."
     """
 
-    cache = {sheetName:sheet}
-    erros = []
-    for classe in sheet:
-        proRels = classe.get("proRel")
-        proRelCods = classe.get("processosRelacionados")
-        if proRels and proRelCods:
-            eSintetizadoPor = [proRelCods[i] for i,x in enumerate(proRels) if x=="eSintetizadoPor"]
-            for c in eSintetizadoPor:
-                fileName = re.search(r'^\d{3}', c).group(0)
-                if fileName not in cache:
-                    with open(f"files/{fileName}.json","r") as f:
-                        cache[fileName] = json.load(f)
-                # Econtrar o processo em questão
-                sintetizado = [x for x in cache[fileName] if x["codigo"]==c]
-
-                # FIXME: A classe menciona uma classe que não existe!
-                if len(sintetizado) == 0:
-                    print("-"*15)
-                    print(classe["codigo"])
-                    print("eSintetizadoPor")
-                    print(c)
-                    print("-"*15)
-                    # Por enquanto ignora-se quando não existe
-                    continue
-
-                proRels2 = sintetizado[0].get("proRel")
-                proRelCods2 = sintetizado[0].get("processosRelacionados")
-                if proRelCods2 and proRels2 and (len(proRelCods2)==len(proRels2)):
-                    eSintetizadoPor2 = [proRelCods2[i] for i,x in enumerate(proRels2) if x=="eSintetizadoPor"]
-                    # Se existe a relação eSintetizadoPor aqui também, não cumpre com o invariante
-                    if classe["codigo"] in eSintetizadoPor2:
-                        erros.append(classe["codigo"])
-
-    print(f"Erros:{len(erros)}")
-    return erros
+    return checkAntissimetrico(sheet,sheetName,"eSintetizadoPor")
 
 
+def rel_4_inv_4(sheet,sheetName):
+    """
+    A função devolve a lista de classes que não cumprem
+    com este invariante:
+
+    "A relação eSucessorDe é antisimétrica."
+    """
+
+    return checkAntissimetrico(sheet,sheetName,"eSucessorDe")
 
 t0 = time.time()
 for sheetName in sheets:
@@ -134,6 +156,9 @@ for sheetName in sheets:
     rel_4_inv_0(file)
     rel_4_inv_1_1(file,sheetName)
     rel_4_inv_3(file,sheetName)
+    rel_4_inv_4(file,sheetName)
+
 
 t1 = time.time()
 print(t1-t0)
+print(allErros)
