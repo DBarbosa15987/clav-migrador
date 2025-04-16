2b75e90b00bd74b838536ea9471f1614bb74d1a8
DBarbosa15987 <dbarbosa15987@gmail.com>
2025-04-16 22:52:34 +0100

    criação de correções para os erros dos invariantes rel_4_inv_12 e rel_4_inv_13

---
+++ queryfix.py
@@ -0,0 +1,38 @@
+import re
+from report import Report,ErroInv
+
+def rel_4_inv_12_fix(allClasses,erros: list[ErroInv]):
+    """
+    Faz a correção das falhas do invariante rel_4_inv_12.
+    """
+
+    for err in erros:
+        classe = allClasses.get(err.cod)
+        if classe:
+            if "legislacao" not in classe:
+                classe["legislacao"] = [err.info]
+            # Aqui evitam-se adicionar legislações repetidas
+            elif err.info not in classe["legislacao"]:
+                classe["legislacao"].append(err.info)
+            # TODO: fazer a mensagem de "fix"
+            err.fix(classe["legislacao"])
+
+
+def rel_4_inv_13_fix(allClasses,erros: list[ErroInv]):
+    """
+    Faz a correção das falhas do invariante rel_4_inv_13.
+    """
+
+    for err in erros:
+        # Neste caso, como os erros são acerca de classes de
+        # nível 4, a correção acontece na classe pai
+        pai = re.search(r'^(\d{3}\.\d{1,3}\.\d{1,3})\.\d{1,4}$', err.cod).group(1)
+        classePai = allClasses.get(pai)
+        if classePai:
+            if "legislacao" not in classePai:
+                classePai["legislacao"] = [err.info]
+            # Aqui evitam-se adicionar legislações repetidas
+            elif err.info not in classePai["legislacao"]:
+                classePai["legislacao"].append(err.info)
+            # TODO: fazer a mensagem de "fix"
+            err.fix(classePai["legislacao"])
